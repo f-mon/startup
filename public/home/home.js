0ff2a14ce15432a9d4d7ed37ae19d7502e67bcc8
Luca Polverini <luca.polverini@it.abb.com>
2014-11-15 15:30:48 +0100

    temporary

--- public/home/home.js
+++ public/home/home.js
@@ -2,52 +2,71 @@
 
 angular.module('rumors.home', ['ngRoute', 'ngResource'])
 
-    .config(['$routeProvider', function ($routeProvider) {
-        $routeProvider.when('/home', {
-            templateUrl: 'home/home.html',
-            controller: 'HomeCtrl'
+  .config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.when('/home', {
+      templateUrl: 'home/home.html',
+      controller: 'HomeCtrl'
+    });
+  }])
+  .factory('rumorsService', ['$resource', '$q', function ($resource, $q) {
+
+    var localRumors = $resource("rumors/locals/:lat/:long");
+
+    return {
+      getRumors: function () {
+        var defer = $q.defer();
+        localRumors.query({
+          lat: 12,
+          long: 23
+        }, function (data) {
+          defer.resolve(data);
         });
-    }])
-    .factory('rumorsService', ['$resource','$q', function ($resource,$q) {
+        return defer.promise;
+      }
+    };
 
-        var localRumors = $resource("rumors/locals/:lat/:long");
+  }])
+  .controller('HomeCtrl', ['$scope', 'rumorsService', '$timeout', function ($scope, rumorsService, $timeout) {
 
-        return {
-            getRumors: function() {
-                var defer = $q.defer();
-                localRumors.query({
-                    lat: 12,
-                    long: 23
-                },function(data) {
-                    defer.resolve(data);
-                });
-                return defer.promise;
-            }
-        };
+    $scope.rumors = [
+      {sender: "LP", msg: "mock message1", db: 12},
+      {sender: "AK", msg: "mock message2", db: 1},
+      {sender: "FF", msg: "this is a message", db: 13},
+      {sender: "FF", msg: "this is a message", db: 23}
+    ];
 
-    }])
-    .controller('HomeCtrl', ['$scope','rumorsService','$timeout', function ($scope,rumorsService,$timeout) {
+    var container = $('#rumors').isotope({
+      itemSelector: '.rumor',
+      layoutMode: 'masonry',
+      masonry: {
+        columnWidth: '.grid-sizer'
+      },
+      getSortData: {
+        name: '.db', // text from querySelector
+        category: '[data-category]', // value of attribute
+        weight: function (itemElem) { // function
+          var weight = $(itemElem).find('.db').text();
+          return parseFloat(weight.replace(/[\(\)]/g, ''));
+        }
+      },
+      sortBy: 'db'
+    })
 
-        $scope.rumors = [
-            {msg:"mock message1"},
-            {msg:"mock message2"},
-            {msg:"mock message3"}
-        ];
+    var mergeRumors = function (newRumors) {
+      container.isotope();
+      console.log(newRumors);
+    };
 
-        var mergeRumors = function(newRumors) {
-          console.log(newRumors);
-        };
+    var pollRumor = function () {
+      $timeout(function () {
+        rumorsService.getRumors().then(function (rumors) {
+          mergeRumors(rumors);
+          pollRumor();
+        });
+      }, 1000, true);
+    };
 
-        var pollRumor = function() {
-            $timeout(function() {
-                rumorsService.getRumors().then(function(rumors) {
-                    mergeRumors(rumors);
-                    pollRumor();
-                });
-            },1000,true);
-        };
+    pollRumor();
 
-        pollRumor();
-
-    }])
+  }])
 ;
