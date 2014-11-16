e3e37f12470070e08a12277b3a627b416fe81873
Luca Polverini <luca.polverini@it.abb.com>
2014-11-16 14:05:10 +0100

    some rumors

--- public/pusher/pusher.js
+++ public/pusher/pusher.js
@@ -6,10 +6,16 @@ app.controller('PusherCtrl', ['$scope','rumorsService','$timeout', function($sco
 
   console.log('pippo')
   var msgs = [
-    {msg: 'Sono seduta la bar qui fuori. Chi mi offre da bere?', time: 4000},
-    {msg: 'Forza Roma!', time: 2500},
-    {msg: 'Forza Lazio!', time: 2500},
+    {msg: 'è in corso la presentazione di Rumors!', time: 3000},
+    {msg: 'Sono seduta la bar qui fuori. Chi mi offre da bere?', time: 6000},
+    {msg: 'è stato un super weekend!', time: 3500},
     {msg: 'Chi vuole venire a fare una corsetta con me?', time: 5000},
+    {msg: 'Android lollipop è una figata pazzesca', time: 2500},
+    {msg: 'Da Zio Pepe una pizza ed una birra a sole 5€ #ziopepe', time: 3000},
+    {msg: 'Avete visto un bassotto nero? è scappato!', time: 3500},
+    {msg: 'La Elena si tromba Simone!!! :)', time: 1000},
+    {msg: 'Vi piace Rumors?', time: 1000},
+    {msg: 'Vendo la mia bicicletta Canondale X5000, sono qui al bar', time: 2000},
   ];
 
  var pushingRumor = function(text){
